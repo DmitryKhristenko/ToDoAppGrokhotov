dbc789de61844e8841989da1298f025755b4c6e0
Dmitry Khristenko <dmitrykhrist@gmail.com>
2024-06-18 18:28:47 +0300

    Initial Commit

---
+++ ToDoAppGrokhotov/ToDoAppGrokhotovApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppGrokhotovApp.swift
+//  ToDoAppGrokhotov
+//
+//  Created by Дмитрий Х on 18.06.24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppGrokhotovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
